18bc9be323f13b9d5066efeed61c9727f45ae027
KristineBerzina <k.beerzina@gmail.com>
2021-06-21 19:16:46 +0300

    Additional info

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,19 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
   //    Number declarations
 
         int firstNumber;
-        int secondNumber;
-        int answer;
+        float secondNumber;
+        float answer;
 
         // Number initialized
-        firstNumber = 9;
-        secondNumber = 3;
+        firstNumber = 22;
+        secondNumber = 7;
         answer = 0;
 
         //Mathematical operator
@@ -27,6 +29,30 @@ public class Main {
             answer = firstNumber / secondNumber;
         }
 
-        System.out.println(answer);
+  //     System.out.println(answer);
+
+        int five = 5;
+        //five += 1; // five = five +1
+
+  //      System.out.println(five++); //post-increment - increase by 1 after using the value
+
+        // five = 6 now
+
+  //      System.out.println(++five); // pre-increment - increase by 1 before using the value
+
+        boolean sixLessThan = 6<3;
+        boolean fiveGreaterThan = 5>9;
+
+        // && - AND - only works if both statements are true
+        // || - OR - works if either statement is true, only gives FALSE if both are false
+        // ! - NOT
+        // != - NOT EQUAL
+        // == - EQUALS
+        if(sixLessThan || fiveGreaterThan) {
+            System.out.println("OR works");
+        } else {
+            System.out.println("OR was skipped");
+        }
+
     }
 }
